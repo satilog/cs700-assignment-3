9050c3b003ba1bb9468551e0aaf025449c3c04e3
Sathyajit Loganathan <loganathansathyajit@gmail.com>
2023-10-15 12:44:46 -0600

    Fix interest being added twice to transaction list

--- src/SavingsAccount.cpp
+++ src/SavingsAccount.cpp
@@ -61,28 +61,6 @@ void SavingsAccount::addInterest()
         this->setBalance(this->getBalance() + interest);
     }
 
-
-    if (this->getBalance() > 0)
-    {
-        Date interestStartDate = this->transactions[0]->getDate();
-
-        // Transaction *lastInterestTransactionPtr = nullptr;
-        for (Transaction *currentTransactionPtr : this->transactions)
-        {
-            if (currentTransactionPtr->getTransactionType() == "interest")
-            {
-                interestStartDate = currentTransactionPtr->getDate();
-            }
-        }
-
-        Date currentDate(time(0));
-        int dateDifferenceInDays = currentDate - interestStartDate;
-        double interest = this->getBalance() * dateDifferenceInDays / 365 * this->getCustomer()->getSavingsInterest();
-
-        Transaction *newInterest = new Transaction("interest", interest, this->getBalance(), currentDate);
-        this->transactions.push_back(newInterest);
-        this->setBalance(this->getBalance() + interest);
-    }
 };
 
 SavingsAccount::~SavingsAccount()
